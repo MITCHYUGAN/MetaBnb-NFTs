5109cdf729a69945194565712d7546112a0bc016
Mitchyugan <denmichigan123@gmail.com>
2022-11-14 13:04:11 +0100

    Running App on netlify

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ const App = () =>{
     <div className='App'>
           <HeaderModal />
             <Routes>
-                <Route path="/MetaBnb-NFTs" element={<PartnersInspirationMetabnbNftsLandingpage />} />
-                <Route path='/MetaBnb-NFTs/Placetostay' element={<PlaceToStay />} />
+                <Route path="/" element={<PartnersInspirationMetabnbNftsLandingpage />} />
+                <Route path='/Placetostay' element={<PlaceToStay />} />
             </Routes>
           <Footer />
     </div>
